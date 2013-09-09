77453704662d9b7a853e1ba2113b166db7328bec
Kingdon Barrett <yebyen@gmail.com>
2013-09-09 02:07:58 +0000

    add validation_tests in model/greeting

--- src/model/greeting.erl
+++ src/model/greeting.erl
@@ -1,2 +1,7 @@
 -module(greeting, [Id, GreetingText]).
 -compile(export_all).
+validation_tests() ->
+  [{fun() -> length(GreetingText) > 0 end,
+      "Greeting must be non-empty!"},
+   {fun() -> length(GreetingText) =< 140 end,
+      "Greeting must be tweetable"}].
